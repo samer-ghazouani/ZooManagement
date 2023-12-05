95cf1f308eaad155dad8bab409737368f0e65c0c
Samer Ghazouani <samer.ghazouani@gmail.com>
2023-12-05 23:29:54 +0100

    init commit

--- src/main/Test.java
+++ src/main/Test.java
@@ -1,34 +1,33 @@
 package main;
 
-import entities.Employe;
-import entities.SocieteArrayList;
+import entities.*;
+import java.util.TreeSet;
 
 public class Test {
     public static void main(String[] args) {
-        Employe e1=new Employe(1,"1","1","12",1);
-        Employe e2=new Employe(2,"2","2","12",2);
-        Employe e3=new Employe(3,"3","3","34",2);
-        Employe e4=new Employe(4,"4","4","34",1);
-        Employe e5=new Employe(5,"5","5","5",1);
-        SocieteArrayList list = new SocieteArrayList();
-        list.ajouterEmploye(e5);
-        list.ajouterEmploye(e4);
-        list.ajouterEmploye(e3);
-        list.ajouterEmploye(e2);
-        list.ajouterEmploye(e1);
+        Departement d1 = new Departement(1,"1",1);
+        Departement d2 = new Departement(2,"2",2);
+        Departement d3 = new Departement(3,"3",3);
+        Departement d4 = new Departement(4,"4",4);
+        Departement d5 = new Departement(5,"5",5);
+        DepartementHashSet set = new DepartementHashSet();
+        set.ajouterDepartement(d3);
+        set.ajouterDepartement(d5);
+        set.ajouterDepartement(d2);
+        set.ajouterDepartement(d4);
+        set.ajouterDepartement(d1);
         System.out.println("Premier Ajout :");
-        list.displayEmploye();
-        System.out.println("Existe-t-il un employe avec le nom '1' : "+list.rechercherEmploye("1"));
-        Employe e6=new Employe(6,"5","6","6",6);
-        System.out.println("Est-ce que l'employe e6 appartient à la liste: "+list.rechercherEmploye(e6));
-        list.supprimerEmploye(e5);
-        System.out.println("Apres la suppression du e5 :");
-        list.displayEmploye();
+        set.displayDepartement();
+        System.out.println("Existe-t-il un département nommé '1' ? : "+set.rechercherDepartement("1"));
+        Departement d6 = new Departement(6,"6",6);
+        System.out.println("Est-ce-que d6 appartient à notre set ? : "+set.rechercherDepartement(d6));
+        set.supprimerDepartement(d2);
+        System.out.println("Apres la suppression du d2 :");
+        set.displayDepartement();
+        TreeSet<Departement> triSet = set.trierDepartementById();
         System.out.println("Apres le tri par id :");
-        list.trierEmployeParId();
-        list.displayEmploye();
-        System.out.println("Apres le tri par nom de departement et grade :");
-        list.trierEmployeParNomDepartementEtGrade();
-        list.displayEmploye();
+        System.out.println("La liste des departements :");
+        for (Departement dept:triSet)
+            System.out.println("-"+dept);
     }
 }
